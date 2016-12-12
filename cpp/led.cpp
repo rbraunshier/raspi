401ef5894c978c1ab64cd5283adffc4cdb35c006
rbraunshier <rene.braunshier@brg-landeck.at>
2016-12-12 22:03:40 +0100

    Update led.cpp

--- cpp/led.cpp
+++ cpp/led.cpp
@@ -9,12 +9,12 @@ return 1;
 pinMode (1, OUTPUT );
 // Start einer Endlosschleife
 while (1) {
-// Spannung einschalten , LED leuchtet .
+// Spannung einschalten , LED leuchtet .PIN 1 >> HIGH 1
 digitalWrite(1, 1);
 // 1 s warten . Wartezeit wird in ms angegeben .
 delay (1000) ;
-// Spannung ausschalten . LED geht aus.
-digitalWrite(0, 0);
+// Spannung ausschalten . LED geht aus. PIN 1 >> LOW 0
+digitalWrite(1, 0);
 // Wieder 1 s warten
 delay (1000) ;
 }

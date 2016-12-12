1ac0b635336b569c7dca5fa2e1a95065bbb2d6ea
rbraunshier <rene.braunshier@brg-landeck.at>
2016-12-12 21:55:44 +0100

    Create led.cpp

---
+++ cpp/led.cpp
@@ -0,0 +1,21 @@
+// Hier binden wir WiringPi ein sowie die C-Standard Ein -/ Ausgabe
+#include <wiringPi .h>
+#include <stdio .h>
+int main () {
+// Initialisieren der WiringPi API
+if ( wiringPiSetup() == -1)
+return 1;
+// Setze GPIO 17 (= WiringPi Pin 0) als Ausgangspin
+pinMode (0, OUTPUT );
+// Start einer Endlosschleife
+while (1) {
+// Spannung einschalten , LED leuchtet .
+digitalWrite(0, 1);
+// 1 s warten . Wartezeit wird in ms angegeben .
+delay (1000) ;
+// Spannung ausschalten . LED geht aus.
+digitalWrite(0, 0);
+// Wieder 1 s warten
+delay (1000) ;
+}
+}
